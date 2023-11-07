4de2bd3c66d9e16611c22942bf16f2f1b1efd0cd
joshbivens <thejoshbivens@gmail.com>
2023-11-07 13:27:27 -0700

    Team: Add form contents

--- OmdahlBivensSanchezP7/MyForm.cpp
+++ OmdahlBivensSanchezP7/MyForm.cpp
@@ -1,2 +1,21 @@
+/*Section done by:
+Name: Alexsia Omdahl
+Email: aomdahl1@cnm.edu
+Program: C++ Craps Game Group Project
+File Name: OmdahlBivensSanchezP7*/
+
 #include "MyForm.h"
 
+using namespace System;
+using namespace System::Windows::Forms;
+
+
+[STAThread]
+void Main()
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	OmdahlBivensSanchezP7::MyForm form;
+	Application::Run(% form);
+}
